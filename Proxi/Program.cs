581ebefe266015962d5450740e1582322a5dc11a
dmytro-fbl <dmytrowork06@gmail.com>
2026-03-27 17:48:14 +0200

    create pattern proxi

---
+++ Proxi/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Proxi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string safeFile = "document.txt";
+            string secretFile = "passwords.secret";
+
+            File.WriteAllText(safeFile, "Привіт!\nЦе тестовий файл.\nВін має три рядки.");
+            File.WriteAllText(secretFile, "Супер секретні дані");
+
+            ITextReader realReader = new SmartTextReader();
+
+            Console.WriteLine(" ТЕСТ 1: Логування (SmartTextChecker)");
+            ITextReader loggingProxy = new SmartTextChecker(realReader);
+
+            char[][] textData = loggingProxy.ReadText(safeFile);
+
+
+            Console.WriteLine("\nТЕСТ 2: Обмеження доступу (SmartTextReaderLocker)");
+
+            ITextReader lockingProxy = new SmartTextReaderLocker(realReader, @"\.secret$");
+
+            Console.WriteLine($"Спроба прочитати: {safeFile}");
+            lockingProxy.ReadText(safeFile);
+
+            Console.WriteLine($"Спроба прочитати: {secretFile}");
+            lockingProxy.ReadText(secretFile);
+
+            File.Delete(safeFile);
+            File.Delete(secretFile);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Proxi/SmartTextChecker.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxi
+{
+    public class SmartTextChecker : ITextReader
+    {
+        private ITextReader _reader;
+        public SmartTextChecker(ITextReader reader)
+        {
+            _reader = reader;
+        }
+        
+        public char[][] ReadText(string filePath)
+        {
+            Console.WriteLine($"Відкритий файл: {filePath}");
+
+            char[][] result = _reader.ReadText(filePath);
+
+            Console.WriteLine($"файл успішно прочитано");
+
+            int totalLines = result.Length;
+            int totalChars = 0;
+            foreach (var line in result)
+            {
+                totalChars += line.Length;
+            }
+
+            Console.WriteLine($" Загальна кількість рядків: {totalLines}");
+            Console.WriteLine($" Загальна кількість символів: {totalChars}");
+            Console.WriteLine($" Закриття файлу: {filePath}");
+            
+
+            return result;
+        }
+    }
+}
