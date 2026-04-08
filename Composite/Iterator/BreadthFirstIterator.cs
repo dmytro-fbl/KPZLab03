1e8b6f15a1a14bf751f5f844b3956685580f65fa
dmytro-fbl <dmytrowork06@gmail.com>
2026-04-08 22:15:27 +0300

    create Iterator

---
+++ Composite/Iterator/BreadthFirstIterator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite.Iterator
+{
+    public class BreadthFirstIterator : ILightNodeIterator
+    {
+        private Queue<LightNode> _queue = new Queue<LightNode>();
+
+        public BreadthFirstIterator(LightNode root)
+        {
+            if(root != null)
+            {
+                _queue.Enqueue(root);
+            }
+        }
+        public bool HasNext()
+        {
+            return _queue.Count > 0;
+        }
+
+        public LightNode Next()
+        {
+            if (!HasNext()) return null;
+            LightNode current = _queue.Dequeue();
+
+            if(current is LightElementNode element)
+            {
+                foreach(var child in element.Children)
+                {
+                    _queue.Enqueue(child);
+                }
+            }
+
+            return current;
+        }
+    }
+}
---
+++ Composite/Iterator/DepthFirstIterator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite.Iterator
+{
+    public class DepthFirstIterator : ILightNodeIterator
+    {
+        private Stack<LightNode> _stack = new Stack<LightNode>();
+
+        public DepthFirstIterator(LightNode root)
+        {
+            if(root != null)
+            {
+                _stack.Push(root);
+            }
+        }
+        public bool HasNext()
+        {
+            return _stack.Count > 0;
+        }
+
+        public LightNode Next()
+        {
+            if (!HasNext()) return null;
+
+            LightNode current = _stack.Pop();
+
+            if(current is LightElementNode element)
+            {
+                for(int i = element.Children.Count - 1; i >= 0; i--)
+                {
+                    _stack.Push(element.Children[i]);
+                }
+            }
+            return current;
+        }
+    }
+}
